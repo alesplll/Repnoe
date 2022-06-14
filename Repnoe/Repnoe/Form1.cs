96172a91d0157905276f2406d37c9bc9e597c493
J0shlerB0y <artembereznoj482@gmail.com>
2022-06-14 12:35:39 +0300

    reinstalled

--- Repnoe/Repnoe/Form1.cs
+++ Repnoe/Repnoe/Form1.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Repnoe
+namespace repnoe
 {
     public partial class Form1 : Form
     {
